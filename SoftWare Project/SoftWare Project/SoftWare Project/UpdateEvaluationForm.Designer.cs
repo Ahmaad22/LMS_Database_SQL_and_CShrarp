1483c5ef6c2bfdb516e3fea936153a8772d87b97
Ahmaad22 <ahmaad.ali.abdulrahman@gmail.com>
2023-08-25 00:29:25 +0300

    first commit

---
+++ SoftWare Project/SoftWare Project/SoftWare Project/UpdateEvaluationForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace SoftWare_Project
+{
+    partial class UpdateEvaluationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.TeamNumberTextBox = new System.Windows.Forms.TextBox();
+            this.SSN = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StudentsComboBox = new System.Windows.Forms.ComboBox();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.SetEvaluationTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.Color.Gray;
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SearchButton.Location = new System.Drawing.Point(966, 217);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(126, 59);
+            this.SearchButton.TabIndex = 42;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // TeamNumberTextBox
+            // 
+            this.TeamNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamNumberTextBox.Location = new System.Drawing.Point(689, 154);
+            this.TeamNumberTextBox.Name = "TeamNumberTextBox";
+            this.TeamNumberTextBox.Size = new System.Drawing.Size(403, 32);
+            this.TeamNumberTextBox.TabIndex = 41;
+            // 
+            // SSN
+            // 
+            this.SSN.AutoSize = true;
+            this.SSN.BackColor = System.Drawing.Color.Silver;
+            this.SSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SSN.ForeColor = System.Drawing.Color.Black;
+            this.SSN.Location = new System.Drawing.Point(685, 124);
+            this.SSN.Name = "SSN";
+            this.SSN.Size = new System.Drawing.Size(115, 20);
+            this.SSN.TabIndex = 40;
+            this.SSN.Text = "Team Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Silver;
+            this.label5.Font = new System.Drawing.Font("Montserrat SemiBold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Coral;
+            this.label5.Location = new System.Drawing.Point(677, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(431, 69);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Evaluate Teams";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(685, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Student IDs";
+            // 
+            // StudentsComboBox
+            // 
+            this.StudentsComboBox.FormattingEnabled = true;
+            this.StudentsComboBox.Location = new System.Drawing.Point(689, 328);
+            this.StudentsComboBox.Name = "StudentsComboBox";
+            this.StudentsComboBox.Size = new System.Drawing.Size(403, 24);
+            this.StudentsComboBox.TabIndex = 44;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.BackColor = System.Drawing.Color.Gray;
+            this.UpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UpdateButton.Location = new System.Drawing.Point(966, 544);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(126, 59);
+            this.UpdateButton.TabIndex = 48;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = false;
+            // 
+            // SetEvaluationTextBox
+            // 
+            this.SetEvaluationTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetEvaluationTextBox.Location = new System.Drawing.Point(689, 481);
+            this.SetEvaluationTextBox.Name = "SetEvaluationTextBox";
+            this.SetEvaluationTextBox.Size = new System.Drawing.Size(403, 32);
+            this.SetEvaluationTextBox.TabIndex = 47;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(685, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Set Evaluation";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SoftWare_Project.Properties.Resources.Online_Review_pana;
+            this.pictureBox2.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(622, 653);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 36;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SoftWare_Project.Properties.Resources.pngegg;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // UpdateEvaluationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.SetEvaluationTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StudentsComboBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.TeamNumberTextBox);
+            this.Controls.Add(this.SSN);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "UpdateEvaluationForm";
+            this.Text = "UpdateEvaluationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox TeamNumberTextBox;
+        private System.Windows.Forms.Label SSN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox StudentsComboBox;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.TextBox SetEvaluationTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ SoftWare Project/SoftWare Project/SoftWare Project/UpdateEvaluationForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftWare_Project
+{
+    public partial class UpdateEvaluationForm : Form
+    {
+        public UpdateEvaluationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            AdvisorForm advisorForm = new AdvisorForm();
+            this.Hide();
+            advisorForm.Show();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
